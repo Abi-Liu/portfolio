a90fa022c7b9b0264e1b009c8d0eaee742d72f27
Abi-Liu <abiliu018@gmail.com>
2023-07-26 17:48:32 -0700

    updating portfolio

--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -5,14 +5,16 @@ import JavaScript from "../assets/javascript.png";
 import ReactImg from "../assets/react.png";
 import Node from "../assets/node.png";
 import Material from "../assets/material.png";
-import GitHub from "../assets/github.png";
+import Git from "../assets/git.png";
 import Mongo from "../assets/mongo.png";
+import Typescript from "../assets/typescript.png";
+import Jest from "../assets/jest.png";
 
 const Technologies = () => {
   return (
     <div
       name="technologies"
-      className="bg-[#0a192f] text-gray-300 h-screen w-full"
+      className="bg-[#0a192f] text-gray-300 h-screen w-full pb-10 sm:pb-0"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
@@ -20,38 +22,54 @@ const Technologies = () => {
             Technologies
           </p>
         </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-14">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-5 gap-4 text-center py-14">
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
             <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
             <p className="my-4">HTML</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={CSS} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
             <p className="my-4">CSS</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={JavaScript} alt="HTML icon" />
+            <img
+              className="w-20 mx-auto"
+              src={JavaScript}
+              alt="Typescript icon"
+            />
             <p className="my-4">JAVASCRIPT</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={ReactImg} alt="HTML icon" />
+            <img
+              className="w-20 mx-auto"
+              src={Typescript}
+              alt="javascript icon"
+            />
+            <p className="my-4">TYPESCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
             <p className="my-4">REACT</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={GitHub} alt="HTML icon" />
-            <p className="my-4">GITHUB</p>
+            <img className="w-20 mx-auto" src={Git} alt="Git icon" />
+            <p className="my-4">Git</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Node} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={Node} alt="Node icon" />
             <p className="my-4">NODE JS</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={Mongo} alt="Mongodb icon" />
             <p className="my-4">MONGO DB</p>
           </div>
           <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
-            <img className="w-20 mx-auto" src={Material} alt="HTML icon" />
+            <img className="w-20 mx-auto" src={Material} alt="MUI icon" />
             <p className="my-4">MATERIAL UI</p>
+          </div>
+          <div className="shadow-md shadow-{#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Jest} alt="Jest icon" />
+            <p className="my-4">JEST</p>
           </div>
         </div>
       </div>
--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import FinanceMe from "../assets/projects/financeme.gif";
 import Jokr from "../assets/projects/Jokr.gif";
 import Socials from "../assets/projects/Socials.gif";
+import ValorantFinder from "../assets/projects/ValorantFinder.gif";
 
 const Work = () => {
   return (
@@ -12,31 +13,36 @@ const Work = () => {
             Work
           </p>
           <p className="py-6">
-            // Please allow for slow load up times due to free hosting
+            // Please allow for slow load up times of up to 2 minutes due to
+            free hosting
           </p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid sm:grid-cols-2 gap-4 max-w-[90%]">
           <div
-            style={{ backgroundImage: `url(${Jokr})` }}
+            style={{ backgroundImage: `url(${ValorantFinder})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Jokr
+                Valorant Finder
                 <br />
               </span>
               <span className="text-l font-bold text-white tracking-wider">
-                A full-stack joke sharing platform. <br /> Technologies: <br />
-                Node.js Express Mongoose Ejs Bootstrap5
+                An application that tracks users match statistics and history in
+                the popular game Valorant. Users can sign up, find teammates to
+                queue with, and leave reviews
+                <br />
+                Technologies: <br />
+                Typescript Node.js Express Mongoose React Jest MaterialUI
               </span>
               <div className="pt-8 text-center">
-                <a href="https://jokr-uvzc.onrender.com/">
+                <a href="https://valorantfinder.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/Abi-Liu/Jokr">
+                <a href="https://github.com/Abi-Liu/Valorant-Team-Finder">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -44,6 +50,38 @@ const Work = () => {
               </div>
             </div>
           </div>
+
+          <div
+            style={{ backgroundImage: `url(${FinanceMe})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                FinanceMe
+                <br />
+              </span>
+              <span className="text-l font-bold text-white tracking-wider">
+                A banking app where users can log in and connect their bank
+                accounts using the Plaid API's secure connection to easily
+                manage their finances. <br />
+                Technologies: <br />
+                Node.js Express Mongoose React Plaid MaterialUI
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://finance-me.netlify.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/Abi-Liu/Finance-Me">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
           <div
             style={{ backgroundImage: `url(${Socials})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
@@ -72,29 +110,27 @@ const Work = () => {
               </div>
             </div>
           </div>
+
           <div
-            style={{ backgroundImage: `url(${FinanceMe})` }}
+            style={{ backgroundImage: `url(${Jokr})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                FinanceMe
+                Jokr
                 <br />
               </span>
               <span className="text-l font-bold text-white tracking-wider">
-                A banking app where users can log in and connect their bank
-                accounts using the Plaid API's secure connection to easily
-                manage their finances. <br />
-                Technologies: <br />
-                Node.js Express Mongoose React Plaid MaterialUI
+                A full-stack joke sharing platform. <br /> Technologies: <br />
+                Node.js Express Mongoose Ejs Bootstrap5
               </span>
               <div className="pt-8 text-center">
-                <a href="https://finance-me.netlify.app/">
+                <a href="https://jokr-uvzc.onrender.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/Abi-Liu/Finance-Me">
+                <a href="https://github.com/Abi-Liu/Jokr">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
