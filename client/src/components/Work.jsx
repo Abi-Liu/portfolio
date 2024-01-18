dce21443cb7558fbc8364eb72d9ea55e06fdd709
Abi Liu <abiliu018@gmail.com>
2024-01-18 15:50:01 -0800

    update portfolio

--- client/src/components/Work.jsx
+++ client/src/components/Work.jsx
@@ -3,10 +3,11 @@ import FinanceMe from "../assets/projects/financeme.gif";
 import Jokr from "../assets/projects/Jokr.gif";
 import Socials from "../assets/projects/Socials.gif";
 import ValorantFinder from "../assets/projects/ValorantFinder.gif";
-import ToyScript from '../assets/projects/ToyScript.png'
+import Budgetbuddy from "../assets/projects/homepage.png";
+import ToyScript from "../assets/projects/ToyScript.png";
 import { FaGithub } from "react-icons/fa";
 import { FaMedium } from "react-icons/fa6";
-import {AiFillEye} from "react-icons/ai"
+import { AiFillEye } from "react-icons/ai";
 
 const Work = () => {
   return (
@@ -24,21 +25,58 @@ const Work = () => {
 
         <div className="grid sm:grid-cols-2 gap-4 max-w-[90%]">
           <div
+            style={{ backgroundImage: `url(${Budgetbuddy})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-lg font-bold text-white tracking-wider">
+                Budget Buddy
+                <br />
+              </span>
+              <p className="text-sm font-semibold text-white tracking-wider">
+                A new and improved version of my previous app, FinanceMe. This
+                iteration utilizes new technologies to improve user experience
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Typescript, React, Node, Express, Docker,
+                Postgres, Redis, Websockets, Nginx, AWS
+              </p>
+              <div className="pt-6 text-center">
+                <a href="https://www.bbapi.online">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <AiFillEye />
+                  </button>
+                </a>
+                <a href="https://github.com/Abi-Liu/Spendify">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    <FaGithub />
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
             style={{ backgroundImage: `url(${ToyScript})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-lg font-bold text-white tracking-wider">
                 ToyScript
                 <br />
               </span>
-              <span className="text-l font-bold text-white tracking-wider">
-                A custom programming language I made from scratch using JavaScript. Implemented key features, such as primitive data types, variables, functions, and conditional statements and more.
-                <br />
-                Technologies: <br />
-                Javascript, Jest
-              </span>
-              <div className="pt-8 text-center">
+              <p className="text-sm font-semibold text-white tracking-wider">
+                A custom programming language I made from scratch using
+                JavaScript. Implemented key features, such as primitive data
+                types, variables, functions, and conditional statements and
+                more.
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Javascript, Jest
+              </p>
+              <div className="pt-6 text-center">
                 <a href="https://medium.com/@abiliu018/how-i-created-my-own-programming-language-from-scratch-9b99fa0a802b">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     <FaMedium />
@@ -58,18 +96,21 @@ const Work = () => {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-lg font-bold text-white tracking-wider">
                 Valorant Finder
                 <br />
               </span>
-              <span className="text-l font-bold text-white tracking-wider">
+              <p className="text-sm font-semibold text-white tracking-wider">
                 An application that tracks users match statistics and history in
                 Valorant. Users can sign up, find teammates, and leave reviews
-                <br />
-                Technologies: <br />
-                Typescript Node.js Express Mongoose React Jest MaterialUI
-              </span>
-              <div className="pt-8 text-center">
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Typescript Node.js Express Mongoose React Jest
+                MaterialUI
+              </p>
+
+              <div className="pt-6 text-center">
                 <a href="https://valorantfinder.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     <AiFillEye />
@@ -89,17 +130,20 @@ const Work = () => {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-lg font-bold text-white tracking-wider">
                 FinanceMe
                 <br />
               </span>
-              <span className="text-l font-bold text-white tracking-wider">
-                Users can log in and connect their bank
-                accounts using the Plaid API to easily manage their finances. <br />
-                Technologies: <br />
-                Node.js Express Mongoose React Plaid MaterialUI
-              </span>
-              <div className="pt-8 text-center">
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Users can log in and connect their bank accounts using the Plaid
+                API to easily manage their finances.
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Node.js, Express, Mongoose, React, Plaid,
+                MaterialUI
+              </p>
+              <div className="pt-6 text-center">
                 <a href="https://finance-me.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     <AiFillEye />
@@ -119,15 +163,18 @@ const Work = () => {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-lg font-bold text-white tracking-wider">
                 InstaPic
                 <br />
               </span>
-              <span className="text-l font-bold text-white tracking-wider">
+              <p className="text-sm font-semibold text-white tracking-wider">
                 A full-stack social media app that allows users to share images
-                and comments with friends. <br /> Technologies: <br />
-                Node.js Express Mongoose Ejs Cloudinary Bootstrap5
-              </span>
+                and comments with friends.
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Node.js Express Mongoose Ejs Cloudinary Bootstrap5
+              </p>
               <div className="pt-8 text-center">
                 <a href="https://instapic-umux.onrender.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
@@ -148,14 +195,17 @@ const Work = () => {
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div "
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-lg font-bold text-white tracking-wider">
                 Jokr
                 <br />
               </span>
-              <span className="text-l font-bold text-white tracking-wider">
-                A full-stack joke sharing platform. <br /> Technologies: <br />
-                Node.js Express Mongoose Ejs Bootstrap5
-              </span>
+              <p className="text-sm font-semibold text-white tracking-wider">
+                A full-stack joke sharing platform.
+              </p>
+              <br />
+              <p className="text-sm font-semibold text-white tracking-wider">
+                Technologies: Node.js Express Mongoose Ejs Bootstrap5
+              </p>
               <div className="pt-8 text-center">
                 <a href="https://jokr-uvzc.onrender.com/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
